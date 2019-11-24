a045e802d1cee3e4b85feee50dae5c242cfa6753
xuanli <xuanlia199@163.com>
2019-11-24 22:20:29 +0800

    struct

--- go web编程/go基础/method复习.go
+++ go web编程/go基础/method复习.go
@@ -15,12 +15,18 @@ type iphone struct {
 	system string
 }
 
+type xiaomi struct {
+	phone
+	miui string
+}
+
 func main() {
 	p1 := phone{brand: "apple", cpuNum: 10, mem: 2}
 	ip1 := iphone{phone{"apple", 4, 2}, "ios"}
 	fmt.Println(p1.brand)
 	fmt.Println(ip1.cpuNum)
-
+	mi1 := xiaomi{phone{"xiaomi", 8, 6}, "10.2"}
+	fmt.Println(mi1.miui)
 	ip1.setMem()
 	ip1.sayBrand()
 }
