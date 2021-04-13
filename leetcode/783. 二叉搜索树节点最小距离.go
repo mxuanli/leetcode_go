02f447bca50bc3a4f6047805f02bae475d7d267f
hebohao <hebohao@bfops.cn>
2021-04-13 11:02:22 +0800

    81、783

---
+++ leetcode/783. 二叉搜索树节点最小距离.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDiffInBST(root *TreeNode) int {
+	var tmp []int
+	foo(root, &tmp)
+	r := int(math.Pow10(5) + 1)
+	for i := 1; i < len(tmp); i++ {
+		if tmp[i] - tmp[i - 1] < r {
+			r = tmp[i] - tmp[i - 1]
+		}
+	}
+	return r
+}
+
+func foo(root *TreeNode, tmp *[]int) {
+	if root == nil {
+		return
+	}
+	foo(root.Left, tmp)
+	*tmp = append(*tmp, root.Val)
+	foo(root.Right, tmp)
+}
