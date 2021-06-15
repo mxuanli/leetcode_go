3c7468e54cb8fea65800ead5ad096a0460e3d69c
hebohao <hebohao@bfops.cn>
2021-06-15 09:46:05 +0800

    852

---
+++ leetcode/852. 山脉数组的峰顶索引.go
@@ -0,0 +1,22 @@
+package main
+
+func peakIndexInMountainArray(arr []int) int {
+	start := 0
+	end := len(arr) - 1
+	for start+1 < end {
+		mid := start + (end-start)/2
+		if arr[mid-1] < arr[mid] && arr[mid] > arr[mid+1] {
+			return mid
+		}
+		if arr[mid-1] < arr[mid] && arr[mid] < arr[mid+1] {
+			start = mid
+		} else {
+			end = mid
+		}
+	}
+	if arr[start-1] < arr[start] && arr[start] > arr[start+1] {
+		return start
+	} else {
+		return end
+	}
+}
